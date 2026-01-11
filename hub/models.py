cc7dfac7c45499dbf07c84c5858fc372b1ab21f5
Jakub Paluch <qbusik@gmail.com>
2026-01-11 10:44:36 +0100

    add author to comment model

--- hub/models.py
+++ hub/models.py
@@ -73,6 +73,11 @@ class Comment(models.Model):
         on_delete=models.CASCADE,
         related_name="comments"
     )
+    author = models.ForeignKey(
+        Worker,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
 
     def __str__(self):
         return f"Comment on task: {self.task.name}"
