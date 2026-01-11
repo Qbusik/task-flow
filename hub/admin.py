d83cf949e32ea504c27219e11817807ae4d8f518
Jakub Paluch <qbusik@gmail.com>
2026-01-11 08:47:48 +0100

    register models in admin panel

--- hub/admin.py
+++ hub/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from hub.models import (
+    Worker,
+    TaskType,
+    Task,
+    Position,
+)
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "position",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Task)
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ("name", "priority", "deadline", "is_completed",)
+
+admin.site.register(TaskType)
+admin.site.register(Position)
