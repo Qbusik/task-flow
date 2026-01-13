7b33a32681c87fb8f5ce6f982920e36f8fcd860f
Jakub Paluch <qbusik@gmail.com>
2026-01-13 06:28:33 +0100

    update position list for template

--- hub/views.py
+++ hub/views.py
@@ -82,7 +82,7 @@ def toggle_completed_to_task(request, pk):
 class PositionListView(LoginRequiredMixin, generic.ListView):
     model = Position
     context_object_name = "position_list"
-    paginate_by = 10
+    paginate_by = 8
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(PositionListView, self).get_context_data(**kwargs)
